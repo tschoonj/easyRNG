7e322059d2a7b459d230c3e695b50432741cc8bb
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-06-27 13:48:41 +0100

    Restrict exported symbols

--- src/easy_randist.cpp
+++ src/easy_randist.cpp
@@ -28,6 +28,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include "config.h"
 #include "easy_randist.h"
 #include "easy_rng_private.h"
 #include <random>
